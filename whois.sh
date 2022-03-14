e301af78f859a48433a3f872f3014a533fc636ef
hackingyseguridad <antonio_taboada@hotmail.es>
2022-03-14 08:36:29 +0100

    Update whois.sh

--- whois.sh
+++ whois.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+#!/bin/bash
 # Extraer el titualar de IP con whois
 # Listado de IP en fichero ip.txt
 echo
 
 for i in `cat ip.txt`
 do
-        echo "--------------------------------------------------------------" & echo "IP" $i & host $i
+        echo "--------------------------------------------------------------" & echo "IP" $i & host $i; dig -x $i +short;
         sudo whois $i |grep descr:
+        nmap $i -Pn -sT -p 443,8443,8080,8888 --open --script=ssl-cert |grep DNS:
 done
+
