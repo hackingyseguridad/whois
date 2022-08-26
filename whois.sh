a3ed19cb62b20db8663d4e0744c51733c02654c6
hackingyseguridad <antonio_taboada@hotmail.es>
2022-08-26 12:55:04 +0200

    Update whois.sh

--- whois.sh
+++ whois.sh
@@ -8,8 +8,8 @@ for i in `cat ip.txt`
 do
         echo "--------------------------------------------------------------" & echo "IP  ===> " $i " <===" & host $i; dig -x $i +short;
         sudo whois $i |grep descr:
-        nmap $i -Pn -sT -p 443,8443,8080,8888 --open --script=ssl-cert |grep DNS:
-        nmap $1 -sL -R --dns-servers 194.179.1.100
+        nmap $i -Pn -sT -p 443,8443,8080,8888,10443 --open --script=ssl-cert |grep DNS:
+        nmap $i -sL -R --dns-servers 194.179.1.100
         nmap $i -Pn -sT -p0 --script=whois-ip --script-args whodb=nofile
 done
 
