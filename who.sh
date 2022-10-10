c0e6cbfa147c1db5b0c44865625e441539787418
hackingyseguridad <antonio_taboada@hotmail.es>
2022-10-10 09:30:50 +0200

    Update who.sh

--- who.sh
+++ who.sh
@@ -1,3 +1,5 @@
+
+# (c) hacking y seguridad .com 2022
 echo "=================================================================" & echo "IP" $1 & host $1; dig -x $1 +short;
 sudo whois $1 |grep descr:
 nmap $1 -Pn -sT -p 143,443,8443,8080,8888 --open --script=ssl-cert |grep DNS:
