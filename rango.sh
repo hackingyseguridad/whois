09ff2e685eeb821721b517174e5cfde9840bfd36
hackingyseguridad <antonio_taboada@hotmail.es>
2023-05-26 11:12:47 +0200

    Update rango.sh

--- rango.sh
+++ rango.sh
@@ -3,4 +3,4 @@ echo
 echo "Uso.: sh rango.sh 192.168.1.0/24"
 # (c) hackingyseguridad.com 2023
 echo "=================================================================" & echo "rango: " $1 
-nmap $1 -Pn -sT -p 143,443,8000,7443,8443,8080,8888,10443 --open --script=ssl-cert |grep DNS:
+nmap $1 -Pn -sT -p 143,443,587,465,8000,7443,8443,8080,8888,10443 --open --script=ssl-cert |grep DNS:
