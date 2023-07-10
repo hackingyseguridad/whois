0cfcf74ff1aaf6198414535b004c34f66c956a30
hackingyseguridad <antonio_taboada@hotmail.es>
2023-07-10 20:52:45 +0200

    Update whois2.sh

--- whois2.sh
+++ whois2.sh
@@ -1,1 +1,1 @@
-nmap -iL ip.txt  $1, $2 -Pn -sT -R  -p 443,8443,8080,8888,10443 --open --script=ssl-cert -oG resultado.txt | grep "Nmap\|DNS:"
+nmap -iL ip.txt  $1, $2 -Pn -sT -R  -p 110,993,995,143,443,587,465,8000,7443,8443,8080,8888,10443  --open --script=ssl-cert -oG resultado.txt | grep "Nmap\|DNS:"
