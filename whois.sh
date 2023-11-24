56d743dd5c8a741913b2f2b4bea3cc19d15b1f81
hackingyseguridad <antonio_taboada@hotmail.es>
2023-11-24 08:32:43 +0100

    Update whois.sh

--- whois.sh
+++ whois.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-# (c) hacking y seguridad .com 2023
+# (C) Hacking y Seguridad .Com Noviembre 2023
+# ALDEA DEL FRESNO ( MADROD )
 # Extraer el titualar de IP con whois
 # Listado de IP en fichero ip.txt
 echo
 
 for i in `cat ip.txt`
 do
-        echo && echo "--------------------------------------------------------------"
-        echo "--------------------------------------------------------------" & echo "IP  ===> " $i " <===" & host $i; dig -x $i +short;
+        echo && echo "--------------------------------------------------------------" & echo "IP  ===> " $i " <===" && echo "--------------------------------------------------------------" & host $i; dig -x $i +short;
         sudo whois $i |grep descr:
-        nmap $i $2 -Pn -sT -R --dns-servers 194.179.1.100  -p 443,8443,8080,8888,10443 --open --script=ssl-cert -oG resultado.txt | grep "Nmap\|DNS:"
+        nmap $i $2 -Pn -sT -R --dns-servers 213.0.184.85 -p  110,993,995,143,443,587,465,8000,7443,8443,8080,8888,10443 --open --script=ssl-cert -oG resultado.txt | grep "Nmap\|DNS:"
         nmap $i -Pn -sT -p0 --script=whois-ip --script-args whodb=nofile
 done
+
+
 
 
 
