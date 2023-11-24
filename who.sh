ab15ae6761786a87f280c68c7b59814dab0df144
hackingyseguridad <antonio_taboada@hotmail.es>
2023-11-24 08:44:50 +0100

    Update who.sh

--- who.sh
+++ who.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# (c) hacking y seguridad .com 2022
-echo && echo "================================================================="
-echo "=================================================================" & echo "IP" $1 & host $1; dig -x $1 +short;
-sudo whois $1 |grep descr:
-nmap $1 -Pn -sT -p 110,993,995,143,587,465,443,8000,8443,8080,8888,10443 --open --script=ssl-cert |grep DNS:
-nmap $1 -sL -R --dns-servers 194.179.1.100 
-nmap $1 -p0 --script whois-ip --script-args whodb=nofile
+# (c) Hacking y Seguridad .com 2023
+echo && echo "--------------------------------------------------------------" & echo "IP  ===> " $i " <===" && echo "--------------------------------------------------------------" & host $i; dig -x $i +short;
+sudo whois $i |grep descr:
+nmap $i $2 -Pn -sT -R --dns-servers 213.0.184.85 -p  110,993,995,143,443,587,465,8000,7443,8443,8080,8888,10443 --open --script=ssl-cert -oG resultado.txt | grep "Nmap\|DNS:"
+nmap $i -Pn -sT -p0 --script=whois-ip --script-args whodb=nofile
