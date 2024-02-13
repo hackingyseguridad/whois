a916e8a0c26ee074f5c7738f3cab7f174724e637
hackingyseguridad <antonio_taboada@hotmail.es>
2024-02-13 11:41:39 +0100

    Update iprango.sh

--- iprango.sh
+++ iprango.sh
@@ -3,5 +3,4 @@ echo
 echo "Uso.: sh iprango.sh 8.8.8.8"
 # (c) hackingyseguridad.com 2024
 echo "=================================================================" & echo "rango de la IP : " $1 
-echo
 whois $1 $2 |grep route:
